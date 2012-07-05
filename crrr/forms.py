7a244a9c7fd98afb1538f655a60ebf2a137e0cb2
Scott Sturdivant <scott.sturdivant@gmail.com>
2012-07-04 21:02:28 -0600

    Refactor to have an Address form.

--- crrr/forms.py
+++ crrr/forms.py
@@ -14,15 +14,17 @@ from wtforms import (
 class ApplicationForm(Form):
     pass
 
-class PersonalInfo(Form):
-    first_name = TextField("First Name", [validators.Length(min=1, max=25)])
-    last_name  = TextField("Last Name", [validators.Length(min=1, max=35)])
+class Address(Form):
     addr_1     = TextField("Address Line 1", [validators.Length(min=1, max=128)])
     addr_2     = TextField("Address Line 2", [validators.Optional(), validators.Length(max=128)])
     city       = TextField("City", [validators.Length(min=1, max=128)])
-    email      = TextField("Email", [validators.Email()])
-    zip_code   = TextField("Zip Code", [validators.Regexp('^(\d{5}(-\d{4})?)?$')])
     state      = SelectField("State", [validators.Required()], choices=states)
+    zip_code   = TextField("Zip Code", [validators.Regexp('^(\d{5}(-\d{4})?)?$')])
+
+class PersonalInfo(Address):
+    first_name = TextField("First Name", [validators.Length(min=1, max=25)])
+    last_name  = TextField("Last Name", [validators.Length(min=1, max=35)])
+    email      = TextField("Email", [validators.Email()])
     phone      = TextField("Phone", 
                  [validators.Regexp('^(\(?([2-9][0-8][0-9])\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4}))?$')])
 
