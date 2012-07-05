56e019d5f5a7544f233084facc43bdc518c0a771
Scott Sturdivant <scott.sturdivant@gmail.com>
2012-07-04 20:30:05 -0600

    Move test to tests dir.

--- crrr/forms.py
+++ crrr/forms.py
@@ -1,31 +1,25 @@
 import re
-from wtforms import Form, IntegerField, TextField, PasswordField, validators
-
-
-class Phone(object):
-    def __init__(self,
-                 regex='^\(?([2-9][0-8][0-9])\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4})$',
-                 message=None):
-        if message is not None:
-            message = u'Not a valid telephone (NNN) NNN-NNNN.'
-        self.message = message
-        self.regex = regex
-
-    def __call__(self, form, field):
-        return validators.Regexp(self.regex)
+from wtforms import Form, IntegerField, TextField, SelectMultipleField, PasswordField, validators
 
 
 class ApplicationForm(Form):
     pass
 
-class VolunteerForm(Form):
+class PersonalInfo(Form):
     first_name = TextField("First Name", [validators.Length(min=1, max=25)])
     last_name  = TextField("Last Name", [validators.Length(min=1, max=35)])
     addr_1     = TextField("Address Line 1", [validators.Length(min=1, max=128)])
     addr_2     = TextField("Address Line 2", [validators.Optional(), validators.Length(max=128)])
     city       = TextField("City", [validators.Length(min=1, max=128)])
     email      = TextField("Email", [validators.Email()])
-    zip_code   = TextField("Zip Code", [validators.Regexp('^\d{5}(-\d{4})?$')])
-    phone      = TextField("Phone",
-                 [#validators.Optional(),
-                  validators.Regexp('^\(?([2-9][0-8][0-9])\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4})$')])
+    zip_code   = TextField("Zip Code", [validators.Regexp('^(\d{5}(-\d{4})?)?$')])
+    phone      = TextField("Phone", 
+                 [validators.Regexp('^(\(?([2-9][0-8][0-9])\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4}))?$')])
+
+
+class VolunteerForm(PersonalInfo):
+    duty = SelectMultipleField("Duty", 
+                               choices=[('drive', 'Drive some doggies.'),
+                                        ('food',  'Provide some doggie food.'),
+                                        ('check', 'Home checks.')]
+                              )
