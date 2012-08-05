45dff76463566d1a7cbc5456f0db45bc448509d3
Scott Sturdivant <scott.sturdivant@gmail.com>
2012-08-04 20:50:49 -0600

    The copyright year is now function generated.

--- crrr/__init__.py
+++ crrr/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask.ext.mail import Mail
+from datetime import datetime
+
+def get_year():
+    return datetime.now().year
+
 app = Flask(__name__)
 app.config['MAIL_FAIL_SILENTLY '] = False
 app.config['CRRR_EMAIL'] = 'adoptions@coloradorhodesianridgebackrescue.org'
+app.jinja_env.globals.update(get_year=get_year)
 mail = Mail(app)
 import crrr.views
