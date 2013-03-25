0b57f8bb88b3dd51dc134c02ba606efa37660102
Scott Sturdivant <scott.sturdivant@absio.com>
2013-03-25 13:33:19 -0600

    Use roles instead of specifiying a user as an admin.

--- crrr/models.py
+++ crrr/models.py
@@ -1,13 +1,18 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from crrr import db
 
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    admin = db.Column(db.Boolean())
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
     active = db.Column(db.Boolean())
+
 
     def __init__(self, username, email, password="changeme", admin=False, active=True):
         self.username = username
@@ -68,4 +73,3 @@ class Dog(db.Model):
 
     def __repr__(self):
         return '<Dog %r: adopted=%s>' % (self.name, self.adopted)
-
