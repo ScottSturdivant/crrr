14ab93c38271f173da979eff3f960369364f183c
Scott Sturdivant <scott.sturdivant@absio.com>
2013-03-27 15:24:42 -0600

    Add repr to address model.

--- crrr/models/address.py
+++ crrr/models/address.py
@@ -1,6 +1,6 @@
 from crrr import db
 
-class AddressLocation(object):
+class Location(object):
     HOME = 'home'
     WORK = 'work'
     VET  = 'vet'
@@ -15,9 +15,9 @@ class Address(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    location = db.Column(db.Enum(AddressLocation.HOME,
-                                 AddressLocation.WORK,
-                                 AddressLocation.VET,
+    location = db.Column(db.Enum(Location.HOME,
+                                 Location.WORK,
+                                 Location.VET,
                                  name='name_location'))
     line_1 = db.Column(db.String(), nullable=False)
     line_2 = db.Column(db.String(), nullable=True)
@@ -25,3 +25,17 @@ class Address(db.Model):
     state = db.Column(db.String(), nullable=False)
     zip = db.Column(db.String(), nullable=False)
 
+    def __repr__(self):
+        if self.line_2:
+            addr = "%s %s %s, %s %s" % (self.line_1,
+                                        self.line_2,
+                                        self.city.capitalize(),
+                                        self.state.upper(),
+                                        self.zip)
+        else:
+            addr = "%s %s, %s %s" % (self.line_1,
+                                     self.city.capitalize(),
+                                     self.state.upper(),
+                                     self.zip)
+
+        return "<%s: %s>" % (self.location.capitalize(), addr)
