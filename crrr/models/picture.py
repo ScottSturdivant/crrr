19e36f7b673a9fffb1d0432aec8b6a41e5ba11dd
Scott Sturdivant <scott.sturdivant@absio.com>
2013-03-27 14:15:23 -0600

    Fix Picture model.
    Inherit from db.Model, not db.model.  Change all string types to be varchar
    length.

--- crrr/models/picture.py
+++ crrr/models/picture.py
@@ -1,14 +1,14 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from crrr import db
 
-class Picture(db.model):
+class Picture(db.Model):
     __tablename__ = 'picture'
 
     id = db.Column(db.Integer, primary_key=True)
     dog_id = db.Column(db.Integer, db.ForeignKey('dog.id'))
-    title = db.Column(db.String(1024), nullable=False, index=True)
-    file_url = db.Column(db.String(1024), nullable=False)
-    thumb_url = db.Column(db.String(1024), nullable=False)
+    title = db.Column(db.String(), nullable=False, index=True)
+    file_url = db.Column(db.String(), nullable=False)
+    thumb_url = db.Column(db.String(), nullable=False)
 
     def __init__(self, title):
         self.title = tile
