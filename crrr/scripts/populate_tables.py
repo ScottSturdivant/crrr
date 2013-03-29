285303500a73250ac988b22b2e0d779090cc55b9
Scott Sturdivant <scott.sturdivant@gmail.com>
2013-03-28 21:37:15 -0600

    Update the import script.
    Allow parsing of the former 'auth' table to give us some users
    with addresses.

--- crrr/scripts/populate_tables.py
+++ crrr/scripts/populate_tables.py
@@ -5,7 +5,11 @@ mysql database and add them to the dev or production databases.
 """
 import argparse
 from crrr import db
-from crrr.models import Dog
+from crrr.models import (
+        Dog,
+        User,
+        Address,
+        )
 
 def str_to_bool(foo):
     return True if foo.lower() in ['y', 'yes', 'true', '1'] else False
@@ -48,6 +52,49 @@ def import_dogs(dog_csv):
         db.session.commit()
         print "Addded %d dogs." % (i + 1)
 
+def import_users(user_csv):
+    """
+    Dump the users info with this cmd:
+    mysql -u crrrescue -ppass crrrescue -B -e "SELECT * FROM auth;" | sed '1d;s/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > users.csv
+    """
+    print "Processing ", user_csv
+    with open(user_csv) as f:
+        for i, line in enumerate(f):
+            uname, password, fname, lname, addr = line.split('","')
+
+            user = User()
+            user.username  = uname.strip('"')
+            user.password  = password
+            user.firstname = fname
+            user.lastname  = lname
+            user.role = 1
+            user.id = i
+
+            # If there was an address associated with a user, add it too
+            addr = addr.replace('"', '').strip()
+            if addr:
+                address = Address()
+                address.user_id = user.id
+                address.state = 'CO'
+                address.location = 'home'
+                if 'Littleton' in addr:
+                    address.line_1 = addr.split('Littleton')[0].strip()
+                    address.city = 'Littleton'
+                    address.zip = addr.split(' ')[-1]
+                elif 'Lakewood' in addr:
+                    address.line_1 = addr.split('Lakewood')[0].strip()
+                    address.city = 'Lakewood'
+                    address.zip = addr.split(' ')[-1]
+                print "Created address: ", address
+
+            user.addresses.append(address)
+
+            print "Created: ", user
+
+            db.session.add(user)
+        db.session.commit()
+        print "Added %d users." % (i + 1)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -56,6 +103,8 @@ def main():
                         action='store_true')
     parser.add_argument('--dog-csv',
                         help='The csv file containing the Dog_info table.')
+    parser.add_argument('--user-csv',
+                        help='The csv file containing the auth table.')
     args = parser.parse_args()
 
     if args.recreate:
@@ -65,6 +114,8 @@ def main():
         db.create_all()
     if args.dog_csv:
         import_dogs(args.dog_csv)
+    if args.user_csv:
+        import_users(args.user_csv)
 
 
 if __name__ == '__main__':
