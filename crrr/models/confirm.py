e19cac8700d0398120b4381ae3d763e02c2e1e2f
Scott Sturdivant <scott.sturdivant@absio.com>
2013-03-29 15:10:12 -0600

    Adding a model to track acct confirmations.

---
+++ crrr/models/confirm.py
@@ -0,0 +1,16 @@
+from crrr import db
+
+
+class Confirm(db.Model):
+    """
+    A table to hold user confirmation hashes.
+
+    To be used when a user registers, an email will be sent to their account
+    which they click on, taking them to a page that allows them to confirm
+    their account and be signed in.
+    """
+    __tablename__ = 'confirm'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    hash = db.Column(db.String(), nullable=False)
