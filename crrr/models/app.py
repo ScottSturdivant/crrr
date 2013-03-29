3742080c3180c6c5f4b4206dcc6ec943d0341fca
Scott Sturdivant <scott.sturdivant@gmail.com>
2013-03-28 21:30:44 -0600

    Add a model for the application itself.

---
+++ crrr/models/app.py
@@ -0,0 +1,34 @@
+from crrr import db
+from datetime import datetime
+
+class Status(object):
+    NEW          = 'new'
+    INIT         = 'initial contact made'
+    ARRANGE_HC   = 'arranging hc'
+    HC_COMPLETED = 'hc completed'
+    APPROVED     = 'approved'
+    UNAPPROVED   = 'unapproved'
+
+
+class App(db.Model):
+    __tablename__ = 'app'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    dog_id = db.Column(db.Integer, db.ForeignKey('dog.id'))
+    assignee_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    submittal_date = db.Column(db.DateTime, default=datetime.now)
+    status = db.Column(db.Enum(Status.NEW,
+                               Status.INIT,
+                               Status.ARRANGE_HC,
+                               Status.HC_COMPLETED,
+                               Status.APPROVED,
+                               Status.UNAPPROVED,
+                               name='app_status'),
+                               default=Status.NEW)
+    archive = db.Column(db.Boolean, default=False)
+
+    # relationships
+    applicant = db.relationship('User', foreign_keys=[user_id], backref='apps')
+    assignee  = db.relationship('User', foreign_keys=[assignee_id])
+
