08e18ec6e4f515198cb40f837bbe57ce1c4b7365
Scott Sturdivant <scott.sturdivant@gmail.com>
2013-04-04 14:30:20 -0600

    Take a stab at tocifying.

--- crrr/static/scripts.js
+++ crrr/static/scripts.js
@@ -20,3 +20,9 @@ over = function() {
     }
 }
 if (window.attachEvent) window.attachEvent("onload", over);
+
+//Executes your code when the DOM is ready.  Acts the same as $(document).ready().
+$(function() {
+    //Calls the tocify method on your HTML div.
+    $("#toc").tocify({ selectors: "h3" });
+});
