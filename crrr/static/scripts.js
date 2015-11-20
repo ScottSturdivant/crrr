4c6ce5d560021f264a40d0f56010c9beff03f10a
Scott Sturdivant <scott.sturdivant@gmail.com>
2015-11-20 07:29:52 -0700

    Remove extra google analytics script.

--- crrr/static/scripts.js
+++ crrr/static/scripts.js
@@ -20,11 +20,3 @@ over = function() {
     }
 }
 if (window.attachEvent) window.attachEvent("onload", over);
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-44557923-1', 'crrr.org');
-ga('send', 'pageview');
